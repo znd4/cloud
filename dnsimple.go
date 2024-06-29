d569828035ff9ccde52b071fabc106cdd36b20b8
Zane Dufour <zane@protectai.com>
2024-06-28 23:24:30 -0400

    fix dkim in dnsimple protonmail records

--- dnsimple.go
+++ dnsimple.go
@@ -22,12 +22,12 @@ type ProtonmailRecordInputs struct {
 var protonmailInputMap = map[string]ProtonmailRecordInputs{
 	"znd4.dev": {
 		verificationString: "protonmail-verification=f425a51143cba67038b420d2012840c02db726c3",
-		dkim:               "protonmail.domainkey.dgwsf7fj6my2r3gc3nmtvkfmamr5klvflmeb4kdfe6ojl2asgwula.domains.proton.ch.",
+		dkim:               "dgwsf7fj6my2r3gc3nmtvkfmamr5klvflmeb4kdfe6ojl2asgwula",
 		dmarc:              "v=DMARC1; p=quarantine",
 	},
 	"znd4.me": {
 		verificationString: "protonmail-verification=bf0b2c0048c78ef0584a1a8fc2cd3cf13828fd3a",
-		dkim:               "protonmail.domainkey.dspg66b3xa4h6ddxwhskf5bbduujpap6l6f7dktkxrrs624p7k34a.domains.proton.ch.",
+		dkim:               "dspg66b3xa4h6ddxwhskf5bbduujpap6l6f7dktkxrrs624p7k34a",
 		dmarc:              "v=DMARC1; p=quarantine",
 	},
 }
@@ -75,12 +75,17 @@ func addProtonmailRecords(ctx *pulumi.Context, domainName string) error {
 	if err != nil {
 		return err
 	}
-	for _, name := range []pulumi.String{"protonmail._domainkey", "protonmail2._domainkey", "protonmail3._domainkey"} {
+	for _, i := range []string{"", "2", "3"} {
+		name := pulumi.String(fmt.Sprintf("protonmail%s._domainkey", i))
 		_, err = dnsimple.NewZoneRecord(ctx, fmt.Sprintf("zane@%s DKIM - %s", domainName, name), &dnsimple.ZoneRecordArgs{
 			Name:     name,
 			ZoneName: pulumi.String(domainName),
 			Type:     pulumi.String("CNAME"),
-			Value:    inputs.dkim,
+			Value: pulumi.String(fmt.Sprintf(
+				"protonmail%s.domainkey.%s.domains.proton.ch.",
+				i,
+				inputs.dkim,
+			)),
 		})
 		if err != nil {
 			return err
